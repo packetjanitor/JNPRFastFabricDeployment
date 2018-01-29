a6fdc9acf9de7c7f3cfbba552bc90def714f8bb8
Mitch Moore <packetjanitor@gmail.com>
2018-01-29 07:05:03 -0800

    update display options

--- Base-Configuration/Build-Fabric-silent.sh
+++ Base-Configuration/Build-Fabric-silent.sh
@@ -10,11 +10,11 @@ ansible-playbook -i 03-IP-Address/all.inv 03-IP-Address/deploy-addresses.yml > /
 printf "Addresses applied.\n"
 sleep 3
 ansible-playbook -i 04-Routing/OSPF/all.inv 04-Routing/OSPF/deploy-ospf.yml > /dev/null
-printf "OSPF Routing Protocol installed\n"
+printf "OSPF Routing Protocol installed.\n"
 sleep 3
 ansible-playbook -i 05-Overlay-Config/Leaf-Only/all.inv 05-Overlay-Config/Leaf-Only/deploy-overlay.yml > /dev/null
 sleep 3
-printf "Overlay deployed\n"
+printf "Overlay deployedi.\n"
 sleep 3
 printf "Letting the hardware form adjacencies and stabilize.\n"
 sleep 3
--- Base-Configuration/Build-Fabric.sh
+++ Base-Configuration/Build-Fabric.sh
@@ -3,16 +3,15 @@
 # Comannd example ./Build-Fabric.sh
 # Tested on Linux 4.4.0-109-generic #132-Ubuntu
 # Date written 2018 January 24
-printf  "Fabric initialization begins\r"
+printf  "Fabric initialization beginsi.\r"
 ansible-playbook -i 03-IP-Address/all.inv 03-IP-Address/deploy-addresses.yml
-printf  "Addresses applied\r"
+printf  "Addresses appliedi.\r"
 sleep 3
 ansible-playbook -i 04-Routing/OSPF/all.inv 04-Routing/OSPF/deploy-ospf.yml
-printf "OSPF Routing Protocol installed\r"
+printf "OSPF Routing Protocol installedi.\r"
 sleep 3
 ansible-playbook -i 05-Overlay-Config/Leaf-Only/all.inv 05-Overlay-Config/Leaf-Only/deploy-overlay.yml
-printf  "Overlay deployed\r"
-printf "Letting the hardware catch up to the software world.\r"
+printf  "Overlay deployed.\r"
 secs=$((3 * 60))
 while [ $secs -gt 0 ]; do
    echo -ne "Countdown to Fabric Availability: "
@@ -20,3 +19,6 @@ while [ $secs -gt 0 ]; do
    sleep 1
    : $((secs--))
 done
+done
+sleep 3
+printf "\n\nThe Fabric is ready.\n"
