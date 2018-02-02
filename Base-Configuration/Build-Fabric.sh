3fd1fea4a02348ead5ca80e4488d1c39adc3fbc5
Mitch Moore <packetjanitor@gmail.com>
2018-02-01 17:13:08 -0800

    Fixed build

--- Base-Configuration/Build-Fabric.sh
+++ Base-Configuration/Build-Fabric.sh
@@ -3,12 +3,12 @@
 # Comannd example ./Build-Fabric.sh
 # Tested on Linux 4.4.0-109-generic #132-Ubuntu
 # Date written 2018 January 24
-printf  "Fabric initialization beginsi.\r"
+printf  "Fabric initialization begins.\r"
 ansible-playbook -i 03-IP-Address/all.inv 03-IP-Address/deploy-addresses.yml
-printf  "Addresses appliedi.\r"
+printf  "Addresses applied.\r"
 sleep 3
 ansible-playbook -i 04-Routing/OSPF/all.inv 04-Routing/OSPF/deploy-ospf.yml
-printf "OSPF Routing Protocol installedi.\r"
+printf "OSPF Routing Protocol installed.\r"
 sleep 3
 ansible-playbook -i 05-Overlay-Config/Leaf-Only/all.inv 05-Overlay-Config/Leaf-Only/deploy-overlay.yml
 printf  "Overlay deployed.\r"
@@ -18,7 +18,6 @@ while [ $secs -gt 0 ]; do
    echo -ne "$secs\033[0K\r"
    sleep 1
    : $((secs--))
-done
 done
 sleep 3
 printf "\n\nThe Fabric is ready.\n"
